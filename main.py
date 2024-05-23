36f455efe0236bcbd6097233af0f659488937a09
Laura Luchez <127155012+DevLuchez@users.noreply.github.com>
2024-05-23 20:47:10 -0300

    Cadastro do Gestor - BackEnd Funcional

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import mysql.connector
-from flask_cors import CORS
 from mysql.connector import Error
-import os
-import base64
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-
-app.config['UPLOAD_FOLDER'] = 'uploads'
-
 
 # Função para conectar ao banco de dados
 def conectar_bd():
@@ -18,8 +12,8 @@ def conectar_bd():
         conexao = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='admin123',
-            database='banco'
+            password='',
+            database='amigosemacao'
         )
         print("Conexão ao banco de dados MySQL bem-sucedida")
         return conexao
@@ -27,97 +21,112 @@ def conectar_bd():
         print(f"Erro ao conectar ao banco de dados: {e}")
         return None
 
-# Rota para criar um novo evento
-@app.route('/evento-post', methods=['POST'])
-def criar_evento():
-    conexao = conectar_bd()
-    if not conexao:
-        return jsonify({'error': 'Erro ao conectar ao banco de dados'}), 500
-
-    nome_evento = request.form.get('nome_evento')
-    data_evento = request.form.get('data_evento')
-    imagem_evento = request.files['imagem_evento']
-
-    # Verifica se todos os campos necessários foram enviados
-    if not (nome_evento and data_evento and imagem_evento):
-        return jsonify({'error': 'Todos os campos são obrigatórios!'}), 400
-
-    try:
-        cursor = conexao.cursor()
-        imagem_bytes = imagem_evento.read()
-        imagem_base64 = base64.b64encode(imagem_bytes).decode('utf-8')
-
-        cursor.execute("INSERT INTO evento (nome_evento, data_evento, imagem_evento) VALUES (%s, %s, %s)", (nome_evento, data_evento, imagem_base64))
-        conexao.commit()
-        cursor.close()
-        conexao.close()
-
-        return jsonify({'message': 'Evento criado com sucesso!'}), 201
-    except Error as e:
-        print(f"Erro ao criar evento: {e}")
-        return jsonify({'error': 'Erro ao criar evento'}), 500
-
-@app.route('/evento-get', methods=['GET'])
-def obter_eventos():
+# Rota para renderizar a página principal com a lista de gestores
+@app.route('/')
+def index():
     conexao = conectar_bd()
     if not conexao:
         return jsonify({'error': 'Erro ao conectar ao banco de dados'}), 500
 
     try:
         cursor = conexao.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM evento")
-        eventos = cursor.fetchall()
+        cursor.execute("SELECT * FROM gestores")
+        gestores = cursor.fetchall()
         cursor.close()
         conexao.close()
-        return jsonify(eventos), 200
+        return render_template('index.html', gestores=gestores)
     except Error as e:
-        print(f"Erro ao obter eventos: {e}")
-        return jsonify({'error': 'Erro ao obter eventos'}), 500
+        print(f"Erro ao obter gestores: {e}")
+        return jsonify({'error': 'Erro ao obter gestores'}), 500
 
-# Rota para atualizar um evento existente
-@app.route('/evento-update/<int:id>', methods=['PUT'])
-def atualizar_evento(id):
+# Rota para criar um novo gestor
+@app.route('/gestor-post', methods=['POST'])
+def criar_gestor():
     conexao = conectar_bd()
     if not conexao:
         return jsonify({'error': 'Erro ao conectar ao banco de dados'}), 500
 
-    nome_evento = request.form.get('nome_evento')
-    data_evento = request.form.get('data_evento')
+    nome = request.form.get('nomeGestor')
+    email = request.form.get('emailGestor')
+    pin = request.form.get('pinGestor')
 
-    if not (nome_evento and data_evento):
+    if not (nome and email and pin):
         return jsonify({'error': 'Todos os campos são obrigatórios!'}), 400
 
     try:
         cursor = conexao.cursor()
-        cursor.execute("UPDATE evento SET nome_evento = %s, data_evento = %s WHERE idEvento = %s", (nome_evento, data_evento, id))
+        cursor.execute("INSERT INTO gestores (nomeGestor, emailGestor, pinGestor) VALUES (%s, %s, %s)", (nome, email, pin))
         conexao.commit()
         cursor.close()
         conexao.close()
 
-        return jsonify({'message': 'Evento atualizado com sucesso!'}), 200
+        return jsonify({'message': 'Gestor criado com sucesso!'}), 201
     except Error as e:
-        print(f"Erro ao atualizar evento: {e}")
-        return jsonify({'error': 'Erro ao atualizar evento'}), 500
+        print(f"Erro ao criar gestor: {e}")
+        return jsonify({'error': 'Erro ao criar gestor'}), 500
 
-@app.route('/evento-delete/<int:id>', methods=['DELETE'])
-def deletar_evento(id):
+# Rota para obter todos os gestores
+@app.route('/gestor-get', methods=['GET'])
+def obter_gestores():
+    conexao = conectar_bd()
+    if not conexao:
+        return jsonify({'error': 'Erro ao conectar ao banco de dados'}), 500
+
+    try:
+        cursor = conexao.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM gestores")
+        gestores = cursor.fetchall()
+        cursor.close()
+        conexao.close()
+        return jsonify(gestores), 200
+    except Error as e:
+        print(f"Erro ao obter gestores: {e}")
+        return jsonify({'error': 'Erro ao obter gestores'}), 500
+
+# Rota para atualizar um gestor existente
+@app.route('/gestor-update/<int:id>', methods=['PUT'])
+def atualizar_gestor(id):
+    conexao = conectar_bd()
+    if not conexao:
+        return jsonify({'error': 'Erro ao conectar ao banco de dados'}), 500
+
+    nome = request.form.get('nomeGestor')
+    email = request.form.get('emailGestor')
+    pin = request.form.get('pinGestor')
+
+    if not (nome and email and pin):
+        return jsonify({'error': 'Todos os campos são obrigatórios!'}), 400
+
+    try:
+        cursor = conexao.cursor()
+        cursor.execute("UPDATE gestores SET nomeGestor = %s, emailGestor = %s, pinGestor = %s WHERE idGestor = %s", (nome, email, pin, id))
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+
+        return jsonify({'message': 'Gestor atualizado com sucesso!'}), 200
+    except Error as e:
+        print(f"Erro ao atualizar gestor: {e}")
+        return jsonify({'error': 'Erro ao atualizar gestor'}), 500
+
+# Rota para deletar um gestor existente
+@app.route('/gestor-delete/<int:id>', methods=['DELETE'])
+def deletar_gestor(id):
     conexao = conectar_bd()
     if not conexao:
         return jsonify({'error': 'Erro ao conectar ao banco de dados'}), 500
 
     try:
         cursor = conexao.cursor()
-
-        # Exclui o evento do banco de dados
-        cursor.execute("DELETE FROM evento WHERE idEvento = %s", (id,))
+        cursor.execute("DELETE FROM gestores WHERE idGestor = %s", (id,))
         conexao.commit()
         cursor.close()
         conexao.close()
 
-        return jsonify({'message': 'Evento excluído com sucesso!'}), 200
+        return jsonify({'message': 'Gestor excluído com sucesso!'}), 200
     except Error as e:
-        print(f"Erro ao excluir evento: {e}")
-        return jsonify({'error': 'Erro ao excluir evento'}), 500
+        print(f"Erro ao excluir gestor: {e}")
+        return jsonify({'error': 'Erro ao excluir gestor'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
